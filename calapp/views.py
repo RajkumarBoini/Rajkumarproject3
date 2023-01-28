73ba8c5d72db97037b61d64a37e0ef623ac4c1f2
Rajkumar Boini <Rajkumarboina06@gmail.com>
2023-01-28 13:42:13 +0530

    Initial commit

---
+++ calapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def input(request):
+    return render(request,'input.html')
+def calculate(request):
+    x=int(request.GET['t1'])
+    y=int(request.GET['t2'])
+    op_type=request.GET['op']
+    if op_type=="add":
+        z=x+y
+    elif op_type=='sub':
+        z=x-y
+    elif op_type=="mul":
+        z=x*y
+    else:
+        z=x/y
+    return HttpResponse("The" +op_type+ "is:"+str(z))
+
